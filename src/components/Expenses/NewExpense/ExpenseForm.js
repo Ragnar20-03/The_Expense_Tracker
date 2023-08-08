fb7e7e20dd7fe8b89c6dca4d824c43e05abb4226
Ragnar20-03 <roshanpp20@gmail.com>
2023-08-08 22:13:29 +0530

    Handling From Submission

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -4,50 +4,78 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
-    // const [enteredTitle , setEnteredTitle] = useState('');
-    // const [enteredAmount , setEnteredAmount] = useState('');
-    // const [enteredDate , setEnteredDate] = useState('');
+    const [enteredTitle , setEnteredTitle] = useState('');
+    const [enteredAmount , setEnteredAmount] = useState('');
+    const [enteredDate , setEnteredDate] = useState('');
 
-   const [userInput , setUserInput] =  useState({
-        enteredTitle :'' ,
-        eneterdAmount : '',
-        enteredDate : ''
-    })
+//    const [userInput , setUserInput] =  useState({
+//         enteredTitle :'' ,
+//         eneterdAmount : '',
+//         enteredDate : ''
+//     })
 
-    const titleChangeHandler = (event) => {
-        // setUserInput({
-        //     ...userInput , 
-        //     enteredTitle : event.target.value
-        // })
-        setUserInput((prevstate) => {
-            return {...prevstate , enteredTitle:event.target.value}
-        })
-    }
-    const amountChangeHandler = (event) => {
-        setUserInput((prevstate) => {
-            return {...prevstate , eneterdAmount:event.target.value}
-        })
-    }
-    const dateChangeHandler = (event) => {
-        setUserInput((prevstate) => {
-            return {...prevstate , enteredDate:event.target.value}
-        })
+    // const titleChangeHandler = (event) => {
+    //     setEnteredTitle(event.target.value);
+    //     // setUserInput((prevstate) => {
+    //     //     return {...prevstate , enteredTitle:event.target.value}
+    //     // })
+    // }
+    // const amountChangeHandler = (event) => {
+    //     // setUserInput((prevstate) => {
+    //     //     return {...prevstate , eneterdAmount:event.target.value}
+    //     // })
+
+    //     setEnteredAmount(event.target.value);
+
+    // }
+    // const dateChangeHandler = (event) => {
+    //     // setUserInput((prevstate) => {
+    //         // return {...prevstate , enteredDate:event.target.value}
+    //     // })
+
+    //     setEnteredDate(event.target.value);
+
+    // }
+
+        const inputChangeHandler = (identifier , value) => {
+                if (identifier === 'title')
+                {
+                    setEnteredTitle(value);
+                }
+                else if (identifier === 'amount')
+                {
+                    setEnteredAmount(value);
+                }
+                else 
+                {
+                    setEnteredDate(value)
+                }
+        }
+
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const expenseData = {
+            title : enteredTitle , amount: enteredAmount , 
+            date : new Date(enteredDate)
+        }
+        console.log(expenseData);
     }
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" onChange={(event) => {inputChangeHandler('title' , event.target.value)}}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step= "0.01" onChange={amountChangeHandler}/>
+                <input type="number" min="0.01" step= "0.01" onChange={(event) => {inputChangeHandler('amount' , event.target.value)}}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min = "2003-01-01" max = "2022 - 12 - 31" />
+                <input type="date" min = "2003-01-01" max = "2022 - 12 - 31"  />
             </div>
         </div>
 
