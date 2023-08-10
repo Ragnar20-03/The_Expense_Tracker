72abb0ce9d918ffd8393d2c09c2ea3a60d44a9b0
Ragnar20-03 <roshanpp20@gmail.com>
2023-08-10 18:01:31 +0530

    Added Filter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItems from './components/ExpenseItems';
-import Expenses from './components/Expenses';
-import NewExpense from './components/Expenses/NewExpense/NewExpense';
+import React, { useState } from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
-    <div className="App">
-      <NewExpense />
-      {/* <h1> Jay Ganesh...</h1> */}
-      <Expenses
-        items = {expenses} />
-
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hello = () => {
+
+    return (
+        <div>
+            Hello Roshan
+        </div>
+    )
+}
+
+export default Hello
